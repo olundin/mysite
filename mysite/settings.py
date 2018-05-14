2ae3654ebb014d3c32c15415416acdb5e07c7809
olundin <c.oskar.lundin@gmail.com>
2018-05-14 19:05:38 +0200

    Add preparations for analytics

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'articles.apps.ArticlesConfig',
+    'analytical',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
